fb1702fb0010addbbe150edcdb50040f05726041
bdahle <bdahle@hey.com>
2021-12-14 15:38:23 +0100

    08e2

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,43 +1,40 @@
 // testing custom hooks
+// 💯 setup function
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
+function setup({initialProps} = {}) {
+  const result = {}
+  function TestComponent(props) {
+    result.current = useCounter(props)
+    return null
+  }
+  render(<TestComponent {...initialProps} />)
+  return result
+}
+
 test('exposes the count and increment/decrement functions', () => {
-  let result
-  function TestComponent() {
-    result = useCounter()
-    return null
-  }
-  render(<TestComponent />)
-
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  const result = setup()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
-test('allows customisation of the initial count', () => {
-  const results = {}
-  function TestComponent(props) {
-    Object.assign(results, useCounter(props))
-    return null
-  }
-  render(<TestComponent initialCount={1} />)
-  expect(results.count).toBe(1)
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 3}})
+  expect(result.current.count).toBe(3)
 })
 
-test('allows customisation of the step', () => {
-  const results = {}
-  function TestComponent(props) {
-    Object.assign(results, useCounter(props))
-    return null
-  }
-  render(<TestComponent step={2} />)
-  act(() => results.increment())
-  expect(results.count).toBe(2)
+test('allows customization of the step', () => {
+  const result = setup({initialProps: {step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
-/* eslint no-unused-vars:0 */
