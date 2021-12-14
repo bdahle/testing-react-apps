187227f86798c5f2559484496515d5e1525c9cff
bdahle <bdahle@hey.com>
2021-12-14 15:15:46 +0100

    08e1

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,35 +2,22 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-const UseCounterComponent = () => {
-  const {count, increment, decrement} = useCounter()
-
-  return (
-    <>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <div data-testid="counter">{count}</div>
-    </>
-  )
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  render(<UseCounterComponent></UseCounterComponent>)
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+  render(<TestComponent />)
 
-  const increment = screen.getByText(/increment/i)
-  const decrement = screen.getByText(/decrement/i)
-  const count = screen.getByTestId(/counter/i)
-
-  expect(count).toHaveTextContent(0)
-  userEvent.click(increment)
-  expect(count).toHaveTextContent(1)
-  userEvent.click(decrement)
-  userEvent.click(decrement)
-  expect(count).toHaveTextContent(-1)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
